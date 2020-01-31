d07f68706fa1f3f9e71e7c86ab80bd3637408456
Temez1 <heleniusteemu@gmail.com>
2020-01-31 16:15:58 +0200

    Added two tests

--- src/components/test.unit.test.js
+++ src/components/test.unit.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
-import { render, cleanup } from "@testing-library/react"
+import { render, cleanup, fireEvent } from "@testing-library/react"
 import SimpleBlog from "../../src/components/test"
 
 afterEach(cleanup)
@@ -21,4 +21,23 @@ test("renders title, author and likes", () => {
   )
   expect(component.container).toHaveTextContent("mikki")
   expect(component.container).toHaveTextContent("5")
+})
+
+test("like button gets pressed twice when you press it twice", () => {
+  const blog = {
+    title: "Component testing is done with react-testing-library",
+    author: "mikki",
+    likes: 5
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog = {blog} onClick = {mockHandler} />
+  )
+  const button = getByText("like")
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
